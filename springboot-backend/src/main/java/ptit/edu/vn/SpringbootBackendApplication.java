a50f09af0b8fe51f0f25c54878fbc99dcbd09857
duyhelloworld <codedaovoiduy@gmail.com>
2023-12-19 21:12:08 +0700

    Fix wrong code

--- springboot-backend/src/main/java/ptit/edu/vn/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/ptit/edu/vn/SpringbootBackendApplication.java
@@ -2,9 +2,7 @@ package ptit.edu.vn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
 public class SpringbootBackendApplication {
     public static void main(String[] args) {
