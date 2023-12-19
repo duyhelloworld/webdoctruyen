bbb7b7a5fcc70f765b221fb41cf12ac07163ad49
duyhelloworld <codedaovoiduy@gmail.com>
2023-12-19 21:08:59 +0700

    Easier to maintain

--- springboot-backend/src/main/java/ptit/edu/vn/entity/TokenDied.java
+++ springboot-backend/src/main/java/ptit/edu/vn/entity/TokenDied.java
@@ -4,35 +4,32 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-@EqualsAndHashCode
-class TokenDiedId {
-    User user;
-    LocalDateTime diedAt;
-}
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@IdClass(TokenDiedId.class)
 public class TokenDied {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Id
     private LocalDateTime diedAt;
 
     @Column(columnDefinition = "text")
     private String token;
+
+    public TokenDied(User user, String token) {
+        this.user = user;
+        this.token = token;
+        this.diedAt = LocalDateTime.now();
+    }
 }
--- springboot-backend/src/main/java/ptit/edu/vn/repository/TokenDiedRepository.java
+++ springboot-backend/src/main/java/ptit/edu/vn/repository/TokenDiedRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ptit.edu.vn.entity.TokenDied;
 
-public interface TokenDiedRepository extends JpaRepository<TokenDied, String>{
+public interface TokenDiedRepository extends JpaRepository<TokenDied, Long>{
     public boolean existsByToken(String token);
 }
