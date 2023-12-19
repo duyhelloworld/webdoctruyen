8619977065c32e198dbf7c8e1fa008016aa6917b
duyhelloworld <codedaovoiduy@gmail.com>
2023-12-19 21:07:55 +0700

    Config CORS global

---
+++ springboot-backend/src/main/java/ptit/edu/vn/configuration/CorsConfig.java
@@ -0,0 +1,19 @@
+package ptit.edu.vn.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)  // tắt cookie
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*");
+    }
+}
