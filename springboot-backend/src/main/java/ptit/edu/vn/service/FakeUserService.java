ab0d230fcc2a2d6af8a3d97f3fe372d611ae953f
duyhelloworld <codedaovoiduy@gmail.com>
2023-12-31 21:37:56 +0700

    dọn code thừa

--- springboot-backend/src/main/java/ptit/edu/vn/service/FakeUserService.java
+++ springboot-backend/src/main/java/ptit/edu/vn/service/FakeUserService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
