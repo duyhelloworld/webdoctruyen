64e306e50ad4aa5b2005e00f16881e982c3f838a
duyhelloworld <codedaovoiduy@gmail.com>
2024-01-16 10:28:11 +0700

    format endline

--- springboot-backend/src/main/java/com/duyhelloworld/configuration/CorsConfig.java
+++ springboot-backend/src/main/java/com/duyhelloworld/configuration/CorsConfig.java
@@ -7,13 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer
-{
+public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings( CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowCredentials(false)  // tắt cookie
+                .allowedOriginPatterns("*", "http://localhost:3000")
+                // .allowCredentials(false)  // tắt cookie
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
     }
